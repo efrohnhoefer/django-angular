13e83a844b71c4b157a965902bdb88e4ed278260
Eric Frohnhoefer <EFrohnhoefer@TrustedCS.com>
2014-02-06 10:28:00 -0500

    Demonstrate broken NgFormValidationMixin

--- examples/server/views.py
+++ examples/server/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+import json
 from django.views.generic.base import TemplateView
 from django.conf import settings
 from server.forms import SubscriptionForm, SubscriptionFormWithNgModel
+from django.http import HttpResponseBadRequest, HttpResponse
 
 
 class NgFormValidationView(TemplateView):
@@ -22,6 +24,16 @@ class NgFormValidationViewWithNgModel(NgFormValidationView):
     template_name = 'subscribe-form-with-model.html'
     form = SubscriptionFormWithNgModel(scope_prefix='subscribe_data')
 
+    def post(self, request, *args, **kwargs):
+        if not request.is_ajax():
+            return HttpResponseBadRequest('Expected an XMLHttpRequest')
+
+        in_data = json.loads(request.body)
+        bound_contact_form = SubscriptionFormWithNgModel(data=in_data)
+        # now validate ‘bound_contact_form’ and use it as in normal Django
+        # bound_contact_form is always invalid
+        import pdb;pdb.set_trace();
+        return HttpResponse(json.dumps({}), content_type="application/json")
 
 class Ng3WayDataBindingView(NgFormValidationViewWithNgModel):
     template_name = 'three-way-data-binding.html'
